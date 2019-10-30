eb2b9baee289d02f66bef32ba7943dd95b4b12ae
PierreMonvoisin <monvoisin.pierre@gmail.com>
2019-10-30 16:16:34 +0100

    Correction de l'exercice en cours

--- questions.js
+++ questions.js
@@ -1,70 +1,66 @@
 /**
- * Exercice sur les chaines de caractères.
- * Trouvez la façon de faire la plus optimal.
- * Il peut y avoir plusieur façon de faire.
- */
+* Exercice sur les chaines de caractères.
+* Trouvez la façon de faire la plus optimal.
+* Il peut y avoir plusieur façon de faire.
+*/
 var tailleString = function (texte) {
-    return texte.length;
+  return texte.length;
 }
 var remplaceECar = function (texte) {
-    return texte.replace("e", " ");
+  return texte.replace("e", " ");
 }
 var concatString = function (texte1, texte2) {
-    return (texte1 + texte2);
+  return texte1.concat(texte2);
 }
 var afficherCar5 = function (texte) {
-    return texte.charAt(4);
+  return texte.charAt(4);
 }
 var afficher9Car = function (texte) {
-    return texte.substr(0, 9);
+  return texte.substring(0, 9);
 }
 var majusculeString = function (texte) {
-    return texte.toUpperCase();
+  return texte.toUpperCase();
 }
 var minusculeString = function (texte) {
-    return texte.toLowerCase();
+  return texte.toLowerCase();
 }
 var SupprEspaceString = function (texte) {
-    return texte.replace(/\s+/g,' ').trim();
+  return texte.trim();
 }
 var IsString = function (texte) {
-    if (typeof texte == 'string') {
-      return true;
-    } else {
-      return false;
-    };
+  return typeof texte == 'string';
 }
 var AfficherExtensionString = function (texte) {
-    return texte.split('.').pop();
+  return texte.split('.').pop();
 }
 var NombreEspaceString = function (texte) {
-    return (texte.split(" ").length - 1);
+  return (texte.split(" ").length - 1);
 }
 var InverseString = function (texte) {
-    return texte.split("").reverse().join("");
+  return texte.split('').reverse().join('');
 }
 
 /**
- * Exercices sur les nombres et les caluls mathématiques
- */
+* Exercices sur les nombres et les caluls mathématiques
+*/
 var calculPuissance = function (x, y) {
-    return (x ** y);
+  return (x ** y);
 }
 var valeurAbsolue = function (nombre) {
-    return Math.abs(nombre);
+  return Math.abs(nombre);
 }
 var valeurAbsolueArray = function (array) {
-	for (let i = 0; i < array.length; i++) {
-		array[i] = Math.abs(array[i]);
-	}
+  for (let i = 0; i < array.length; i++) {
+    array[i] = Math.abs(array[i]);
+  }
   return array;
 }
 var sufaceCercle = function (rayon) {
-    return Math.ceil(Math.PI * rayon ** 2);
+  return Math.ceil(Math.PI * rayon ** 2);
 }
 var hypothenuse = function (ab, ac) {
   return Math.sqrt(ab ** 2 + ac ** 2);
 }
 var calculIMC = function (poids, taille) {
-    return Number((poids / taille ** 2).toFixed(2));
+  return Number((poids / taille ** 2).toFixed(2));
 }
